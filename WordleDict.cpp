89fb304a2bd40d54826769d0aba9af5918b21e2a
radubig <radubig1987@gmail.com>
2022-10-29 21:49:52 +0300

    Separated dictionary from player

---
+++ WordleDict.cpp
@@ -0,0 +1,5 @@
+//
+// Created by radu on 10/29/22.
+//
+
+#include "WordleDict.h"
---
+++ WordleDict.h
@@ -0,0 +1,14 @@
+//
+// Created by radu on 10/29/22.
+//
+
+#ifndef WORDLE_WORDLEDICT_H
+#define WORDLE_WORDLEDICT_H
+
+
+class WordleDict {
+
+};
+
+
+#endif //WORDLE_WORDLEDICT_H
