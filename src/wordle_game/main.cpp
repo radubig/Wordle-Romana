416866dd50e6e94b7acbf6f84dbd9f3c8460639a
radubig <radubig1987@gmail.com>
2022-11-07 18:41:47 +0200

    Refactored main for later use

--- src/wordle_game/main.cpp
+++ src/wordle_game/main.cpp
@@ -1,90 +1,151 @@
 #include <iostream>
 #include "wordle_game.h"
 #include "../_common/patterns.h"
+
 using namespace std;
 
-int main()
+static void StandardPlay();
+static void AutoPlay();
+
+int main(int argc, char** argv)
 {
     try
     {
-        word_dict dict("cuvinte.txt");
-        dict.init();
-
-        wordle_game game(dict);
-        game.reset();
-
-        cout << "Wordle: Incearca sa ghicesti un cuvant de 5 litere!\n"
-                "Vei primi la fiecare incercare indicatii despre ce litere fac parte din cuvant.\n\n"
-                "Esti pregatit? Tasteaza un cuvant de 5 litere:" << endl;
-
-        int pattern_code = -1;
-        int guesses = 0;
-        do
+        if(argc < 2)
         {
-            string guess;
-            cin >> guess;
-
-            if (guess.size() != 5)
-            {
-                cout << "Cuvantul introdus nu are 5 litere!\nIntrodu un alt cuvant:" << endl;
-                continue;
-            }
-
-            bool isValid = true;
-            for (char& c : guess)
-            {
-                if (c >= 'a' && c <= 'z')
-                {
-                    c -= 32;
-                }
-                else if (c < 'A' || c > 'Z')
-                {
-                    isValid = false;
-                    break;
-                }
-            }
-
-            if (!isValid)
-            {
-                cout << "Cuvantul introdus contine si alte caractere in afara de litere!\nIntrodu un alt cuvant:" << endl;
-                continue;
-            }
-
-            pattern_code = game.guess(guess);
-            
-            if (pattern_code == -1)
-            {
-                cout << "Cuvantul introdus nu face parte din dictionar!\nIntrodu un alt cuvant:" << endl;
-                continue;
-            }
-            
-            guesses++;
-            int* pattern = patterns::decode_pattern(pattern_code);
-
-            for (int i=0; i<5; i++)
-            {
-                if (pattern[i] == patterns::GREEN)
-                    cout << guess[i] << ": Corect!\n";
-                else if (pattern[i] == patterns::YELLOW)
-                    cout << guess[i] << ": Alta pozitie!\n";
-                else
-                    cout << guess[i] << ": Nu exista!\n";
-            }
-
-            cout << "Codul raspunsului: " << pattern_code << "\n" << endl;
-            if (pattern_code != GUESSED_PATTERN) cout << "Introdu urmatorul cuvant: " << endl;
-        } while (pattern_code != GUESSED_PATTERN);
-
-        if (guesses == 1)
-            cout << "FELICITARI! Ai ghicit cuvantul in 1 incercare!\n" << endl;
+            StandardPlay();
+        }
         else
-            cout << "FELICITARI! Ai ghicit cuvantul in " << guesses << " incercari!\n" << endl;
-        
-        return 0;
+        {
+            AutoPlay();
+        }
     }
     catch (...)
     {
         cout << "A aparut o eroare." << std::endl;
         return -1;
     }
+
+    return 0;
+}
+
+inline static void StandardPlay()
+{
+    word_dict dict("cuvinte.txt");
+    dict.init();
+
+    wordle_game game(dict);
+    //game.reset();
+
+    cout << "Wordle: Incearca sa ghicesti un cuvant de 5 litere!\n"
+            "Vei primi la fiecare incercare indicatii despre ce litere fac parte din cuvant.\n\n"
+            "Esti pregatit? Tasteaza un cuvant de 5 litere:" << endl;
+
+    int pattern_code = -1;
+    int guesses = 0;
+    do
+    {
+        string guess;
+        cin >> guess;
+
+        if (guess.size() != 5)
+        {
+            cout << "Cuvantul introdus nu are 5 litere!\nIntrodu un alt cuvant:" << endl;
+            continue;
+        }
+
+        bool isValid = true;
+        for (char& c : guess)
+        {
+            if (c >= 'a' && c <= 'z')
+            {
+                c -= 32;
+            }
+            else if (c < 'A' || c > 'Z')
+            {
+                isValid = false;
+                break;
+            }
+        }
+
+        if (!isValid)
+        {
+            cout << "Cuvantul introdus contine si alte caractere in afara de litere!\nIntrodu un alt cuvant:" << endl;
+            continue;
+        }
+
+        pattern_code = game.guess(guess);
+
+        if (pattern_code == -1)
+        {
+            cout << "Cuvantul introdus nu face parte din dictionar!\nIntrodu un alt cuvant:" << endl;
+            continue;
+        }
+
+        guesses++;
+        int* pattern = patterns::decode_pattern(pattern_code);
+
+        for (int i=0; i<5; i++)
+        {
+            if (pattern[i] == patterns::GREEN)
+                cout << guess[i] << ": Corect!\n";
+            else if (pattern[i] == patterns::YELLOW)
+                cout << guess[i] << ": Alta pozitie!\n";
+            else
+                cout << guess[i] << ": Nu exista!\n";
+        }
+
+        cout << "Codul raspunsului: " << pattern_code << "\n" << endl;
+        if (pattern_code != GUESSED_PATTERN) cout << "Introdu urmatorul cuvant: " << endl;
+    } while (pattern_code != GUESSED_PATTERN);
+
+    if (guesses == 1)
+        cout << "FELICITARI! Ai ghicit cuvantul in 1 incercare!\n" << endl;
+    else
+        cout << "FELICITARI! Ai ghicit cuvantul in " << guesses << " incercari!\n" << endl;
+
+}
+inline static void AutoPlay()
+{
+    word_dict dict("cuvinte.txt");
+    dict.init();
+
+    wordle_game game(dict);
+    //game.reset();
+
+    int pattern_code = -1;
+    int guesses = 0;
+    do
+    {
+        string guess;
+        cin >> guess;
+
+        if (guess.size() != 5)
+            throw;
+
+        bool isValid = true;
+        for (char& c : guess)
+        {
+            if (c >= 'a' && c <= 'z')
+            {
+                c -= 32;
+            }
+            else if (c < 'A' || c > 'Z')
+            {
+                isValid = false;
+                break;
+            }
+        }
+        if (!isValid)
+            throw;
+
+        pattern_code = game.guess(guess);
+        if (pattern_code == -1)
+            throw;
+
+        guesses++;
+        int* pattern = patterns::decode_pattern(pattern_code);
+
+    } while (pattern_code != GUESSED_PATTERN);
+
 }
--- src/wordle_player/main.cpp
+++ src/wordle_player/main.cpp
@@ -4,60 +4,112 @@
 
 using namespace std;
 
-int main()
+static void StandardPlay();
+static void AutoPlay();
+
+int main(int argc, char** argv)
 {
     try
     {
-        word_dict dict("cuvinte.txt");
-        dict.init();
-
-        wordle_player player(dict);
-        player.reset();
-
-        int guesses = 1;
-        int in_pattern;
-
-        // Conform first_guess_entropy.txt (03.11.2022) cuvantul cu cea mai mare entropie este:
-        // TAREI (6.41381)
-        string guess = "TAREI";
-
-        cout << "Ghiceste: " << guess << "\nIntrodu codul raspunsului: ";
-        cin >> in_pattern;
-        player.apply_guess(guess, in_pattern);
-        cout << "Cuvinte posibile: ";
-        for (const string& word : player.words_list) cout << word << " ";
-        cout << endl;
-
-        while (in_pattern != GUESSED_PATTERN)
+        if(argc < 2)
         {
-            guesses++;
-
-            if (player.words_list.size() == 1)
-            {
-                cout << "Ghiceste: " << player.words_list[0] << "\n";
-                cout << "Cuvant ghicit in " << guesses << " incercari.\n" << endl;
-                return 0;
-            }
-            
-            guess = player.get_best_guess();
-            cout << "Ghiceste: " << guess << "\nIntrodu codul raspunsului: ";
-            cin >> in_pattern;
-            player.apply_guess(guess, in_pattern);
-            cout << "Cuvinte posibile: ";
-            for (const string& word : player.words_list) cout << word << " ";
-            cout << endl;
+            StandardPlay();
         }
-
-        if (guesses == 1)
-            cout << "Cuvant ghicit in 1 incercare.\n" << endl;
         else
-            cout << "Cuvant ghicit in " << guesses << " incercari.\n" << endl;
-        
-        return 0;
+        {
+            AutoPlay();
+        }
     }
     catch (...)
     {
         cerr << "A aparut o eroare." << endl;
         return -1;
     }
+
+    return 0;
+}
+
+inline static void StandardPlay()
+{
+    word_dict dict("cuvinte.txt");
+    dict.init();
+
+    wordle_player player(dict);
+    //player.reset();
+
+    int guesses = 1;
+    int in_pattern;
+
+    // Conform first_guess_entropy.txt (03.11.2022) cuvantul cu cea mai mare entropie este:
+    // TAREI (6.41381)
+    string guess = FIRST_GUESS;
+
+    cout << "Ghiceste: " << guess << "\nIntrodu codul raspunsului: ";
+    cin >> in_pattern;
+    player.apply_guess(guess, in_pattern);
+    cout << "Cuvinte posibile: ";
+    for (const string& word : player.words_list) cout << word << " ";
+    cout << endl;
+
+    while (in_pattern != GUESSED_PATTERN)
+    {
+        guesses++;
+
+        if (player.words_list.size() == 1)
+        {
+            cout << "Ghiceste: " << player.words_list[0] << "\n";
+            cout << "Cuvant ghicit in " << guesses << " incercari.\n" << endl;
+            return;
+        }
+
+        guess = player.get_best_guess();
+        cout << "Ghiceste: " << guess << "\nIntrodu codul raspunsului: ";
+        cin >> in_pattern;
+        player.apply_guess(guess, in_pattern);
+        cout << "Cuvinte posibile: ";
+        for (const string& word : player.words_list) cout << word << " ";
+        cout << endl;
+    }
+
+    if (guesses == 1)
+        cout << "Cuvant ghicit in 1 incercare.\n" << endl;
+    else
+        cout << "Cuvant ghicit in " << guesses << " incercari.\n" << endl;
+}
+
+inline static void AutoPlay()
+{
+    word_dict dict("cuvinte.txt");
+    dict.init();
+
+    wordle_player player(dict);
+    //player.reset();
+
+    int guesses = 1;
+    int in_pattern;
+
+    // Conform first_guess_entropy.txt (03.11.2022) cuvantul cu cea mai mare entropie este:
+    // TAREI (6.41381)
+    string guess = FIRST_GUESS;
+
+    cin >> in_pattern;
+
+    player.apply_guess(guess, in_pattern);
+
+    while (in_pattern != GUESSED_PATTERN)
+    {
+        guesses++;
+
+        if (player.words_list.size() == 1)
+        {
+            cout << "Ghiceste: " << player.words_list[0] << "\n";
+            return;
+        }
+
+        guess = player.get_best_guess();
+
+        cin >> in_pattern;
+        player.apply_guess(guess, in_pattern);
+    }
+
 }
